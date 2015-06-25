48943474c18759b730729f42009da832dd837977
Kelly Witwicki Faddegon <kelly.witwickifaddegon@gmail.com>
2015-06-25 10:38:22 -0700

    Improve display

--- game.js
+++ game.js
@@ -10,6 +10,7 @@
     // this.dim_x = DIM_X;
     // this.dim_y = DIM_Y;
     this.board = new Pipes.Board(size);
+    $(window).on("resize", this.draw.bind(this));
   };
 
   // var DIM_X = Pipes.DIM_X = 900; // use the lesser of window.height to make square side length, -100px -- responsive??
@@ -33,22 +34,32 @@
   // };
 
   Game.prototype.draw = function draw() {
-    $("#game-canvas").css("height", $(window).height() - 100);
-    $("#game-canvas").css("width", $(window).height() - 100);
+    $("#game-canvas").empty();
+    
+    // factor sizing out into separate method
+
+    var frameLength = $(window).height();
+    if ($(window).width() < $(window).height()) {
+      frameLength = $(window).width();
+    };
+    frameLength -= 200;
+    frameLength = (frameLength < 300) ? 300 : Math.floor(frameLength);
+    $("#game-canvas").css("height", frameLength);
+    $("#game-canvas").css("width", frameLength);
 
     var grid = this.board.grid
     times(grid.length, function(row) {
       var $displayRow = $("<div>").attr("data-row", row)
-      $displayRow.css("height", $("#game-canvas").height() / grid.length)
-      $displayRow.css("width", $("#game-canvas").height())
+      $displayRow.css("height", frameLength / grid.length)
+      $displayRow.css("width", frameLength)
       $displayRow.addClass("row");
       $("#game-canvas").append($displayRow);
       times(grid.length, function(col) {
         var $square = $("<div>").attr("data-row-and-col", row + "," + col)
         $displayRow.append($square);
         $square.addClass("col");
-        $square.css("height", $("#game-canvas").height() / grid.length )
-        $square.css("width", $("#game-canvas").height() / grid.length )
+        $square.css("height", frameLength / grid.length )
+        $square.css("width", frameLength / grid.length )
       });
     });
 
