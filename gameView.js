b89175c33b6959cd17abddab14b6bfc665f11318
Kelly Witwicki Faddegon <kelly.witwickifaddegon@gmail.com>
2015-06-30 18:28:08 -0700

    Styling

--- gameView.js
+++ gameView.js
@@ -5,11 +5,12 @@ if ( window.Pipes === undefined ) {
 };
 
 var GameView = Pipes.GameView = function GameView() {
+  this.color = Pipes.Colors[Math.floor(Math.random()*10 % Pipes.Colors.length)];
   this.size();
-  var color = Pipes.Colors[Math.floor(Math.random()*10 % Pipes.Colors.length)];
-  $("#title").css("color", color)
+  $("#title").css("color", this.color)
   $("#game-container").css("box-shadow",
-                           "0px 0px 0px 1px black, 0px 0px 0px 3px" + color);
+                           "0px 0px 0px 1px black, 0px 0px 0px 3px" + this.color);
+  // $("#game-container").css("background-color", color);
   $(window).on("resize", this.size.bind(this));
 };
 
@@ -25,20 +26,24 @@ GameView.prototype.size = function size() {
   this.frameLength = (this.frameLength < 200) ? 200 : Math.floor(this.frameLength);
   $("#game-container").css("height", this.frameLength);
   $("#game-container").css("width", this.frameLength);
+  $("#title-container").css("width", this.frameLength);
 
   var that = this;
   Object.keys(Pipes.Board.Sizes).forEach(function(size) {
     $button = $("<button>").append(size + "x" + size);
+    $button.css("color", that.color);
     $button.on("click", function () {
       $("#selection").addClass("hidden");
       that.start(size);
     });
     $("#selection .buttons").append($button);
   })
+  // $("#selection .buttons button").css("background-color", this.color);
 };
 
 GameView.prototype.start = function start(size) {
   var game = new Pipes.Game(size, this.frameLength);
+  // $("#game-container").css("background-color", "black");
   game.draw();
 };
 
